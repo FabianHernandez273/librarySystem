1a15a712c20fc9b949215e7e425277c81fdf0877
Fabian Hernandez <fabianhernan168@gmail.com>
2025-10-02 17:44:35 -0500

    Se agregan traducciones personalizadas para validaciones

---
+++ lang/es/validation.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'accepted'             => 'El campo :attribute debe ser aceptado.',
+    'after'                => 'El campo :attribute debe ser una fecha posterior a :date.',
+    'after_or_equal'       => 'El campo :attribute debe ser una fecha posterior o igual a :date.',
+    'before'               => 'El campo :attribute debe ser una fecha anterior a :date.',
+    'before_or_equal'      => 'El campo :attribute debe ser una fecha anterior o igual a :date.',
+    'date'                 => 'El campo :attribute no es una fecha válida.',
+    'required'             => 'El campo :attribute es obligatorio.',
+    'unique'               => 'El campo :attribute ya ha sido tomado.',
+    'max'                  => [
+        'string'  => 'El campo :attribute no debe ser mayor a :max caracteres.',
+    ],
+    'min'                  => [
+        'string'  => 'El campo :attribute debe tener al menos :min caracteres.',
+    ],
+    'attributes' => [
+        'fecha_prestamo'   => 'fecha de préstamo',
+        'fecha_devolucion' => 'fecha de devolución',
+    ],
+];
